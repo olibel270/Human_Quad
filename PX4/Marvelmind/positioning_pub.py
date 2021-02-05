b936b2c2776a6c7f0e81156a3e3c510a0664e4ac
olibel270 <olibel270@gmail.com>
2021-02-05 12:30:57 +0000

    chateau_setup_successful

--- PX4/Marvelmind/positioning_pub.py
+++ PX4/Marvelmind/positioning_pub.py
@@ -11,7 +11,7 @@ from sensor_msgs.msg import Imu
 from marvelmind import MarvelmindHedge
 import types
 
-BCN_OFFSET= 2.70526 #Reference frame offset in rad
+BCN_OFFSET= 2.61799 #Reference frame offset in rad
 
 imu_data = None
 
@@ -30,7 +30,7 @@ def positioning_pub():
 
     pose = PoseStamped()
 
-    hedge = MarvelmindHedge(tty = "/dev/ttyACM0", adr=None, debug=False) # create MarvelmindHedge thread
+    hedge = MarvelmindHedge(tty = "/dev/ttyACM1", adr=None, debug=False) # create MarvelmindHedge thread
     if (len(sys.argv)>1):
         hedge.tty= sys.argv[1]
     hedge.start()
@@ -50,9 +50,9 @@ def positioning_pub():
             pose.header.stamp = rospy.Time.now()
             temp_x = position[2]
             temp_y = position[1]
-            pose.pose.position.y = (temp_x*np.cos(BCN_OFFSET)+temp_y*np.sin(BCN_OFFSET))
-            pose.pose.position.x = -(-temp_x*np.cos(BCN_OFFSET)+temp_y*np.sin(BCN_OFFSET))
-            pose.pose.position.z = position[3]
+            pose.pose.position.x = (temp_x*np.cos(BCN_OFFSET)+temp_y*np.sin(BCN_OFFSET))
+            pose.pose.position.y = -(-temp_x*np.cos(BCN_OFFSET)+temp_y*np.sin(BCN_OFFSET))
+            pose.pose.position.z = -position[3]
 
             pose.pose.orientation = Quaternion(
         	imu_data.orientation.x,
