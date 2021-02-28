0b5c6f6cc8e6a5c847332cce4710520ce8c888e4
olibel270 <olibel270@gmail.com>
2021-02-28 17:20:36 +0000

    Moved some trajectories from arducopter to PX4 here they should live.

--- PX4/offboard_test.py
+++ PX4/offboard_test.py
@@ -25,13 +25,13 @@ def publisher_thread(obj, publisher, rate, fly_time=0):
     print("Iterations: ", iterations)
     while count<iterations:
         publisher.publish(obj)
-        if(count==iterations/(1/5)):#8s
+        if(count==iterations/(1/5)):#4s
             setpoint.pose.position.x -= 1.5
-        if(count==iterations/(2/5)):#12s
+        if(count==iterations/(2/5)):#8s
             setpoint.pose.position.y -= 1.5
-        if(count==iterations/(3/5):#16s
+        if(count==iterations/(3/5):#12s
             setpoint.pose.position.x += 1.5
-        if(count==iterations/(4/5):#20s
+        if(count==iterations/(4/5):#16s
             setpoint.pose.position.y += 1.5
         r.sleep()
         count += 1
